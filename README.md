1e02fb375658d26dcd8eec057798b23a9bab2e76
john-neg <john_neg@mail.ru>
2022-10-06 17:25:44 +0300

    tests and infra changes

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# praktikum_new_diplom
+![foodgram_workflow](https://github.com/john-neg/foodgram-project-react/actions/workflows/foodgram_workflow.yml/badge.svg)
+
+# Foodgram
+
+Продуктовый помощник _(Practicum by Yandex education project)_
+
+На этом сервисе пользователи могут публиковать рецепты, подписываться на публикации других пользователей, добавлять понравившиеся рецепты в список «Избранное», а перед походом в магазин скачивать сводный список продуктов, необходимых для приготовления одного или нескольких выбранных блюд.
+
+Проект доступен по адресу - http://130.193.48.5/
+
+## Технологии:
+
+Foodgram учебный проект базирующийся на:
+- Python 3.7
+- Django 3.2.16
+- Docker 20.10.18
+- Nginx 1.21.3
+- Postgres - 13.0
---
+++ tests/test_foodgram_yml.py
@@ -0,0 +1,50 @@
+import os
+import re
+
+from .conftest import root_dir
+
+
+class TestWorkflow:
+
+    def test_workflow(self):
+        backend_basename = 'foodgram_workflow'
+
+        yaml = f'{backend_basename}.yaml'
+        is_yaml = yaml in os.listdir(root_dir)
+
+        yml = f'{backend_basename}.yml'
+        is_yml = yml in os.listdir(root_dir)
+
+        if not is_yaml and not is_yml:
+            assert False, (
+                f'В каталоге {root_dir} не найден файл с описанием workflow '
+                f'{yaml} или {yml}.\n'
+                '(Это нужно для проверки тестами на платформе)'
+            )
+
+        if is_yaml and is_yml:
+            assert False, (
+                f'В каталоге {root_dir} не должно быть двух файлов {backend_basename} '
+                'с расширениями .yaml и .yml\n'
+                'Удалите один из них'
+            )
+
+        filename = yaml if is_yaml else yml
+
+        try:
+            with open(f'{os.path.join(root_dir, filename)}', 'r') as f:
+                foodgram = f.read()
+        except FileNotFoundError:
+            assert False, f'Проверьте, что добавили файл {filename} в каталог {root_dir} для проверки'
+
+        assert (
+                re.search(r'on:\s*push:\s*branches:\s*-\smaster', foodgram) or
+                'on: [push]' in foodgram or
+                'on: push' in foodgram
+        ), f'Проверьте, что добавили действие при пуше в файл {filename}'
+        assert 'pytest' in foodgram, f'Проверьте, что добавили pytest в файл {filename}'
+        assert 'appleboy/ssh-action' in foodgram, f'Проверьте, что добавили деплой в файл {filename}'
+        assert 'appleboy/telegram-action' in foodgram, (
+            'Проверьте, что настроили отправку telegram сообщения '
+            f'в файл {filename}'
+        )
